90afa5bcc4441be3c47adf0b120822b6a7966460
Daniel <daniel@adaki.net>
2024-02-27 12:28:17 -0400

    Update logger.js

--- config/logger.js
+++ config/logger.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
   transports: [
     new winston.transports.File({
       filename: path.join(__dirname, '..', 'logs', 'strapi.log'),
-      level: 'debug', // Captura desde debug hasta los niveles más críticos
+      level: 'silly', // Captura desde debug hasta los niveles más críticos
     }),
     new winston.transports.Console({
       level: 'silly', 
