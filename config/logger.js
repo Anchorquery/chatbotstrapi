8588805da65a6e090c9607c9113aefbaad32fdb6
Daniel <daniel@adaki.net>
2024-02-27 15:25:08 -0400

    Update logger.js

--- config/logger.js
+++ config/logger.js
@@ -1,26 +1,16 @@
 'use strict';
 
-const { winston, formats: { prettyPrint, levelFilter } } = require('@strapi/logger');
+const winston = require('winston');
 const path = require('path');
 
-module.exports = {
+module.exports = ({ env }) => ({
   transports: [
-    // Transporte para la consola
-    new winston.transports.Console({
-      level: 'silly', // Ajusta para capturar desde debug
-      format: winston.format.combine(
-        levelFilter('silly'), // Usa 'debug' para permitir todo desde debug en adelante
-        prettyPrint({ timestamps: 'YYYY-MM-DD hh:mm:ss.SSS' })
-      ),
-    }),
-    // Transporte para archivo
     new winston.transports.File({
       filename: path.join(__dirname, '..', 'logs', 'strapi.log'),
-      level: 'silly', // Ajusta para capturar desde debug
-      format: winston.format.combine(
-        levelFilter('silly'), // Asegura que el archivo también capture desde debug
-        prettyPrint({ timestamps: 'YYYY-MM-DD hh:mm:ss.SSS' })
-      ),
+      level: 'silly', // Captura desde debug hasta los niveles más críticos
+    }),
+    new winston.transports.Console({
+      level: 'silly', 
     })
   ],
-};
+});
